832539c61addc413e6a3dea213a9fee6ac8e6f79
Md Lutfor Tahman Shifat <shifat151@gmail.com>
2021-08-31 11:31:27 +0600

    table vertical scrollbar added and x-overflow hidden

--- portfolio_proj/settings.py
+++ portfolio_proj/settings.py
@@ -16,8 +16,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+# DEBUG=True
 
 ALLOWED_HOSTS = ['shifat75.pythonanywhere.com']
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
